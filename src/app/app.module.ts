24df344a394d9324c60cb605db4d159c27765ec3
Kogey24 <hammertonkogey@gmail.com>
2024-02-09 17:20:23 +0300

    form complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CommonModule } from '@angular/common';
+import { FormComponent } from "./form/form.component";
+ 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FormComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        CommonModule,
+        BrowserModule,
+        // MatIconModule,
+        //  NgApexchartsModule,
+        // PerfectScrollbarModule,
+        //  MatMenuModule,
+        // MatTooltipModule,
+        // MatProgressBarModule,
+        //  MatTableModule,
+        //  MatPaginatorModule,
+        // MatInputModule,
+        //  MatSortModule,
+        // MatSelectModule,
+        // MatTabsModule,
+        // MatCheckboxModule,
+        //  MatTableExporterModule,
+        //  MatProgressSpinnerModule,
+        // MatFormFieldModule,
+        // MatButtonModule,
+        // MatDialogModule,
+        // MatDatepickerModule,
+        // ComponentsModule,
+        // MatCardModule,
+        ReactiveFormsModule,
+        // MatSnackBarModule,
+        //  MatToolbarModule,
+        // SharedModule,
+        //  MatStepperModule,
+        // NgxMaskModule,
+        //  MatChipsModule,
+        CommonModule,
+        FormsModule,
+        FormComponent
+    ]
 })
 export class AppModule { }
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,45 @@
+import { Component,  OnInit } from '@angular/core';
+import { FormGroup, FormsModule, Validators, FormBuilder } from '@angular/forms';
+
+
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css'],
+})
+export class FormComponent implements OnInit {
+  [x: string]: any;
+  registrationForm!: FormGroup;
+  showForm: boolean = true;
+  formData: any;
+
+ 
+  
+
+  constructor(
+    private fb: FormBuilder,
+  ) {};
+  
+
+  ngOnInit(): void {
+  // create api call
+  // create a form based on api fields
+  this.registrationForm= this.fb.group({
+    fullname:['', Validators.required],
+    contactinfo:['', Validators.required],
+    position:['', Validators.required],
+    department:['',Validators.required],
+    status:[''],
+  })
+
+  }
+    
+    onSubmit(){
+      if(this.registrationForm.valid){
+      console.warn(this.registrationForm.value);
+    } else{
+      this.registrationForm.markAllAsTouched();
+    }
+  }
+}
